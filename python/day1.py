384721c9ed80a6dc498793a6d7e29f5929725902
Giulio Mazzanti <gmazzanti@protonmail.com>
2021-12-14 21:10:20 +0100

    refactor(1): add typing

--- python/day1.py
+++ python/day1.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-def star1(input_file):
+def star1(df: pd.DataFrame) -> int:
     mask = df.diff() > 0
     return df[mask].count()
 
-def star2(input_file):
+def star2(df: pd.DataFrame) -> int:
     mask = df.rolling(3).sum().diff() > 0
     return df[mask].count()
 
 if __name__ == "__main__":
-    df = pd.read_csv("input.txt", header=None).iloc[:, 0]
-    print(f"Star #1: {star1(df)}")
-    print(f"Star #2: {star2(df)}")
+    file_df = pd.read_csv("input.txt", header=None).iloc[:, 0]
+    print(f"Star #1: {star1(file_df)}")
+    print(f"Star #2: {star2(file_df)}")
